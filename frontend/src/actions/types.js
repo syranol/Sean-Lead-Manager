7f8b2cebfc4baa4db8b08274bccf5b9be0d1d05e
Sean Lo <shon4081@gmail.com>
2020-04-21 21:16:22 -0700

    Update Logout function

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -12,7 +12,7 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 
-export const LOGOUT_SUCCESS = "LOGIN_SUCCESS";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
