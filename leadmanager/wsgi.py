aefce9a724f64414222087798cd890bebd4504fa
Sean Lo <shon4081@gmail.com>
2020-04-21 19:40:46 -0700

    Updated venv, requirements.txt

--- leadmanager/wsgi.py
+++ leadmanager/wsgi.py
@@ -7,12 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-
-from whitenoise.django import DjangoWhiteNoise
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leadmanager.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
